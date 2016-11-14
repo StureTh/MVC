0288c561b856e8e0a0d9a2c687f0072f8ae9efb1
StureTh <sturethuren@gmail.com>
2016-11-14 13:55:37 +0100

    Added two test projects. Played around alittle

---
+++ MVC Laborationer/MVC Laborationer/Controllers/HelloWorldController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Laborationer.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        public string Index()
+        {
+            return "This is my <b>default</b> action..";
+        }
+
+        public string Welcome(string name, int numtimes = 1)
+        {
+            
+            return HttpUtility.HtmlEncode($"Hello {name}\n" + $"Numtimes is: {numtimes}");
+        }
+    }
+}
