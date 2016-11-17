c19821d636827f8a61e59a2a46777b24148b1d6f
Sture Thuren <sturethuren@gmail.com>
2016-11-17 17:31:56 +0100

    Started on Labb1. Created PhotoGallery and made it possible to add photos

--- MVC Laborationer/Labb1/Controllers/GalleryController.cs
+++ MVC Laborationer/Labb1/Controllers/GalleryController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.IO;
+using Labb1.Models;
 
 namespace Labb1.Controllers
 {
@@ -11,6 +13,23 @@ namespace Labb1.Controllers
         // GET: Gallery
         public ActionResult Index()
         {
+            var photoList =
+                Directory.EnumerateFiles(Server.MapPath("~/GalleryPhotos"))
+                    .Select(x => "~/GalleryPhotos/" + Path.GetFileName(x)).ToList();
+
+            return View(photoList);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Photo photo, HttpPostedFileBase file)
+        {
+            file.SaveAs(Path.Combine(Server.MapPath("~/GalleryPhotos"), file.FileName));
+
             return View();
         }
     }
---
+++ MVC Laborationer/Labb1/Models/Photo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Labb1.Models
+{
+    public class Photo
+    {
+        public string Name { get; set; }
+
+    }
+}
