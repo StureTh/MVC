49d931ef52abb2182a18702fb29f6d49cf173ad1
Sture Thuren <sturethuren@gmail.com>
2016-11-18 19:36:29 +0100

    Working on login function

---
+++ MVC Laborationer/Labb1/Controllers/AccountController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Labb1.Models;
+
+namespace Labb1.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+
+            public static List<User> userList = new List<User>(); 
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        // Register
+        public ActionResult Register()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(User userAccount)
+        {
+            if (ModelState.IsValid)
+            {
+                userList.Add(userAccount);
+
+                return RedirectToAction("Registerd", new { userName = userAccount.Name });
+            }
+            else
+            {
+                return View(userAccount);
+            }
+        }
+
+        public ActionResult Registerd(string userName)
+        {
+            ViewBag.name = userName;
+            return View();
+        }
+
+
+        // Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(User userAccount)
+        {
+            var user =
+                userList.FirstOrDefault(usr => usr.Name == userAccount.Name && usr.Password == userAccount.Password);
+            if (user != null)
+            {
+                Session["User"] = user;
+                return RedirectToAction("LoggedIn");
+            }
+
+            return View(userAccount);
+
+        }
+
+        public ActionResult LoggedIn()
+        {
+            return View();
+        }
+
+
+
+        
+    }
+}
---
+++ MVC Laborationer/Labb1/Models/User.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Labb1.Models
+{
+    public class User
+    {
+        [Key]
+        public Guid UserId { get; set; }
+
+
+        [Required (ErrorMessage = "Please enter UserName")]
+        public string Name { get; set; }
+
+        [Required (ErrorMessage = "Mail is required")]
+        [RegularExpression(@"^([\w-\.]+)@((\[[0-9]{1,3]\.)|(([\w-]+\.)+))([a-zA-Z{2,4}|[0-9]{1,3})(\]?)$", ErrorMessage = "Please enter valid email.")]
+        public string Mail { get; set; }
+
+
+        [Required(ErrorMessage = "Please enter your password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+
+        [Compare("Password",ErrorMessage = "Password must match")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+
+
+        public bool IsAdmin { get; set; }
+
+    }
+}
