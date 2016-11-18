36cf8f98a1aa05b306a00cf8b75082d492fde570
Sture Thuren <sturethuren@gmail.com>
2016-11-18 17:41:45 +0100

    Created empty MVC project for labb1 and added photogallery

---
+++ MVC Laborationer/Labb1/Controllers/GalleryController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Labb1.Models;
+
+namespace Labb1.Controllers
+{
+    public class GalleryController : Controller
+    {
+        // GET: Gallery
+        public ActionResult Index()
+        {
+            var photoList =
+                Directory.EnumerateFiles(Server.MapPath("~/PhotoGallery"))
+                    .Select(photo => "~/PhotoGallery/" + Path.GetFileName(photo)).ToList();
+            return View(photoList);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+
+        }
+
+        [HttpPost]
+        public ActionResult Create(Photo photo, HttpPostedFileBase file)
+        {
+            file.SaveAs(Path.Combine(Server.MapPath("~PhotoGallery"), file.FileName));
+
+            return View();
+        }
+    }
+}
