ad3f55646ba96b815c2ed97d573cac61f2db3027
Sture Thuren <sturethuren@gmail.com>
2016-12-05 19:07:59 +0100

    Dont work...

---
+++ MVC Laborationer/Labb2/scripts/UploadPhotos.js
@@ -0,0 +1,28 @@
+﻿
+$("form#upload").submit(function (e) {
+    e.preventDefault();
+
+    var formData = new FormData($(this)[0]);
+
+    var photoName = $("#photoName").val();
+    alert(formData);
+    var photodata = {
+        Name: $("#photoName").val()
+    };
+    alert($("#photoName").val());
+   
+    $.ajax({
+        url: "/Album/UploadToAlbum/",
+        type: "POST",
+        data: formData,
+        success: function (data) {
+            alert(data);
+        },
+        cache: false,
+        processData: false,
+        contentType: false
+
+    });
+});
+
+    
